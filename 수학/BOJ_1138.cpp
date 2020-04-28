dc12e16e94ef53858fd7d860227d478ed3ecc230
algoribi <algoribi36@gmail.com>
2020-04-28 16:12:50 +0900

    한 줄로 서기

---
+++ 수학/BOJ_1138.cpp
@@ -0,0 +1,29 @@
+// algorithm_study
+// BOJ_1138_한 줄로 서기
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, an[11] = {0};
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        for (int j = 0; j < n; j++) {
+            if (an[j] == 0 && num == 0) {
+                an[j] = i + 1;
+                break;
+            }
+            if (an[j] == 0)
+                num--;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << an[i] << " ";
+    }
+
+    return 0;
+}

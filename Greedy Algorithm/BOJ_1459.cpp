8f0883e4e48652fd5a6ea4fb6e7b841580dbde84
algoribi <algoribi36@gmail.com>
2020-04-30 21:42:08 +0900

    걷기

---
+++ Greedy Algorithm/BOJ_1459.cpp
@@ -0,0 +1,34 @@
+// algorithm_study
+// BOJ_1459_걷기
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long x, y, w, s, an;
+    cin >> x >> y;
+    cin >> w >> s;
+
+    if (w * 2 <= s)
+        an = (x + y) * w;
+    else {
+        if (x <= y) {
+            an = x * s;
+            if ((y - x) * w > (y - x) * s)
+                an += ((y - x) % 2) * w + ((y - x) - (y - x) % 2) * s;
+            else
+                an += (y - x) * w;
+        } else {
+            an = y * s;
+            if ((x - y) * w > (x - y) * s)
+                an += ((x - y) % 2) * w + ((x - y) - (x - y) % 2) * s;
+            else
+                an += (x - y) * w;
+        }
+    }
+
+    cout << an;
+
+    return 0;
+}

960d57f39ad9a630dcd8bb2c7652332946fe9604
algoribi <algoribi36@gmail.com>
2020-04-30 21:40:34 +0900

    초콜릿 자르기

---
+++ Dynamic programming/BOJ_2163.cpp
@@ -0,0 +1,13 @@
+// algorithm study
+// BOJ_2163_초콜릿 자르기
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << n * m - 1 << '\n';
+    return 0;
+}

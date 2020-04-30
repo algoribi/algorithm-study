567a17f7393dce41f01d93cfbdcc131d06d42be9
algoribi <algoribi36@gmail.com>
2020-04-30 21:40:17 +0900

    1로 만들기

---
+++ Dynamic programming/BOJ_1463.cpp
@@ -0,0 +1,23 @@
+// algorithm study
+// BOJ_1463_1로 만들기
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, dp[1000010];
+    cin >> n;
+
+    dp[1] = 0;
+    for (int i = 2; i < n + 1; i++) {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 2 == 0 && dp[i] > dp[i / 2] + 1)
+            dp[i] = dp[i / 2] + 1;
+        if (i % 3 == 0 && dp[i] > dp[i / 3] + 1)
+            dp[i] = dp[i / 3] + 1;
+    }
+
+    cout << dp[n];
+    return 0;
+}

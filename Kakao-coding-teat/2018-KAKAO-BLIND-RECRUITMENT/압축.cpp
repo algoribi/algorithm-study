3b759ef9dbf06a59afa9bd66cc7b1c00cae21b35
algoribi <algoribi36@gmail.com>
2020-05-07 16:49:59 +0900

    압축

---
+++ Kakao-coding-teat/2018-KAKAO-BLIND-RECRUITMENT/압축.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string msg) {
+    vector<string> dic = {"-", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+                          "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    vector<int> answer;
+    for (int i = 0; i < msg.size(); i++) {
+        int save;
+        string temp = "";
+        while (1) {
+            temp.push_back(msg[i]);
+            auto iter = find(dic.begin(), dic.end(), temp);
+            if (iter != dic.end()) {
+                save = iter - dic.begin();
+                i++;
+            } else {
+                answer.push_back(save);
+                dic.push_back(temp);
+                i--;
+                break;
+            }
+        }
+    }
+    return answer;
+}

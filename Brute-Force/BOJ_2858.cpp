89acdbd256316760469ac31f957cd6d728d53187
algoribi <algoribi36@gmail.com>
2020-05-12 13:41:11 +0900

    기숙사 바닥

---
+++ Brute-Force/BOJ_2858.cpp
@@ -0,0 +1,22 @@
+// algorithm study
+// BOJ_2858_기숙사 바닥
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int r, b, l, w;
+    cin >> r >> b;
+    for (int i = 3; i <= (r + b) / 2; i++) {
+        if ((r + b) % i == 0) {
+            l = (r + b) / i;
+            w = i;
+            if ((w - 2) * (l - 2) == b)
+                break;
+        }
+    }
+
+    cout << l << " " << w;
+    return 0;
+}

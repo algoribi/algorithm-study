d49ce224417b70f0ad414b6cdc4701fa48762574
algoribi <algoribi36@gmail.com>
2020-05-12 13:41:00 +0900

    수 이어 쓰기1

---
+++ Brute-Force/BOJ_1748.cpp
@@ -0,0 +1,19 @@
+// algorithm study
+// BOJ_1748_수 이어 쓰기1
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, answer = 0, i = 1;
+    cin >> n;
+
+    while (i <= n) {
+        answer += (n - i + 1);
+        i *= 10;
+    }
+    cout << answer;
+
+    return 0;
+}

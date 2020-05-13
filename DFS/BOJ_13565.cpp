52c270b39b3f93b22e94a194f807f64217e3b9df
algoribi <algoribi36@gmail.com>
2020-05-13 13:57:32 +0900

    침투

--- DFS/BOJ_13565.cpp
+++ DFS/BOJ_13565.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int m, n, visit[1010][1010] = {0}, chk = 0;
-char map[1010][1010];
+char arr[1010][1010];
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
@@ -20,7 +20,7 @@ void dfs(int x, int y) {
     for (int i = 0; i < 4; i++) {
         int newx = x + dx[i];
         int newy = y + dy[i];
-        if (newx < 0 || newx >= m || newy < 0 || newy >= n || visit[newx][newy] == 1 || map[newx][newy] == '1')
+        if (newx < 0 || newx >= m || newy < 0 || newy >= n || visit[newx][newy] == 1 || arr[newx][newy] == '1')
             continue;
         dfs(newx, newy);
         if (chk == 1)
@@ -32,12 +32,12 @@ int main() {
     cin >> m >> n;
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            cin >> map[i][j];
+            cin >> arr[i][j];
         }
     }
 
     for (int i = 0; i < n; i++) {
-        if (map[0][i] == '0' && visit[0][i] == 0) {
+        if (arr[0][i] == '0' && visit[0][i] == 0) {
             dfs(0, i);
             if (chk == 1) {
                 cout << "YES";

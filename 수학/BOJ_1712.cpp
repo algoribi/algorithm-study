2fe7b910e2bee330e2dfd88a97440c82837319e9
algoribi <algoribi36@gmail.com>
2020-07-03 14:42:47 +0900

    손익분기점

---
+++ 수학/BOJ_1712.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, counter = 1;
+    cin >> a >> b >> c;
+    if (c <= b)
+        cout << -1;
+    else
+        cout << a / (c - b) + 1;
+    return 0;
+}

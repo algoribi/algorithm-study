9c20f43f2220ad4d5a77a63d4dbf6d8317183376
algoribi <algoribi36@gmail.com>
2020-07-03 14:46:19 +0900

    세 수

---
+++ 구현/BOJ_10817.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int num[5];
+    cin>>num[0]>>num[1]>>num[2];
+    sort(num, num + 3);
+    cout<<num[1];
+    return 0;
+}
